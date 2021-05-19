6221dcccdf1e3143fe5c1929dee2cf8171308540
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2021-05-19 02:17:03 -0400

    Added CurTime and DeltaTime utilities
    - Added CAI.Utilities.CurTime and CAI.Utilities.DeltaTime global variables, these are updated every tick.
    - Added CAI.Utilities.GetUID function, it'll add the current time to whatever string is passed as the first argument to create a unique ID.

--- lua/cai/base/utils.lua
+++ lua/cai/base/utils.lua
@@ -2,6 +2,7 @@
 local Utils  = CAI.Utilities
 local format = string.format
 local Key    = "%i %i %i"
+local UID    = "%s-%s"
 
 function Utils.VectorToKey(Vector)
 	return format(Key, Vector:Unpack())
@@ -14,4 +15,20 @@ end
 -- Thank you Garry
 function Utils.DivideVector(A, B)
 	return Vector(A.x / B.x, A.y / B.y, A.z / B.z)
+end
+
+do -- CAI CurTime and DeltaTime
+	Utils.CurTime   = CurTime()
+	Utils.DeltaTime = 0
+
+	hook.Add("Tick", "CAI CurTime/DeltaTime", function()
+		local Time = CurTime()
+
+		Utils.DeltaTime = Time - Utils.CurTime
+		Utils.CurTime   = Time
+	end)
+
+	function Utils.GetUID(Name)
+		return format(UID, Name, Utils.CurTime)
+	end
 end
